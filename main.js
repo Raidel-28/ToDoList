a5b72da3f629fb7b1bfc90c69ac691a0c7d01da1
g-broche <gregory.broche@yahoo.fr>
2025-04-09 11:35:11 +0200

    added script to manage list with add, delete features and a way to change the completion status of a task

--- main.js
+++ main.js
@@ -1,34 +1,79 @@
 let lastTaskId = 0;
 
-class task {
+class Task {
     id;
     label;
     dateAdded;
-    dateDone;
-    isDone;
+    dateDone = null;
+    isDone = false;
     constructor(label) {
         this.id = lastTaskId;
         lastTaskId++;
         this.label = label;
+        this.dateAdded = new Date();
+        console.log(this.dateAdded);
     }
     createTaskElement() {
-        const taskArticle = document.createElement("article")
-        taskArticle.className = "task"
-        const divTaskDescriptionWrapper = document.createElement("div")
-        taskArticle.appendChild(divTaskDescriptionWrapper)
+        const taskItem = document.createElement("li");
+        taskItem.className = this.isDone ? "task done" : "task"
+        const parsedDate = `${this.dateAdded.getFullYear()}/${this.dateAdded.getMonth()}/${this.dateAdded.getDate()} ${this.dateAdded.getHours()}:${this.dateAdded.getMinutes()}`
+        taskItem.innerHTML = `<div>
+            <div class="date">
+                <span class="date-add">${parsedDate}</span>
+                <span class="date-done"></span>
+            </div>
+            <span class="task-name"></span>
+        </div>
+        <div>
+            <button class="check">finie</button>
+            <button class="del">supprimer</button>
+        </div>`;
+        taskItem.querySelector(".task-name").innerText = this.label;
+        taskItem.querySelector(".del").addEventListener("click", () => {
+            taskList.removeTask(this.id);
+        })
+        taskItem.querySelector(".check").addEventListener("click", () => {
+            this.setCompletion(!this.isDone)
+            taskList.refresh()
+        })
+        return taskItem;
     }
-
-    //     <article class="task">
-    //     <div>
-    //         <div class="date">
-    //             <span class="date-add">2025-04-08 16:19</span>
-    //             <span class="date-done">2025-04-08 17:19</span>
-    //         </div>
-    //         <span class="task-name">tâche 1</span>
-    //     </div>
-    //     <div>
-    //         <button class="check">finie</button>
-    //         <button class="del">supprimer</button>
-    //     </div>
-    // </article>
+    setCompletion(isTaskDone) {
+        this.isDone = isTaskDone
+    }
 }
+
+const inputNewTaskLabel = document.getElementById("new-taskfield");
+const buttonAddTask = document.getElementById("add-task");
+
+const taskList = {
+    domElement: document.getElementById("task-list"),
+    tasks: [],
+    addTask(task) {
+        this.tasks.push(task);
+        this.refresh()
+    },
+    removeTask(taskIdToRemove) {
+        this.tasks = this.tasks.filter(task => task.id !== taskIdToRemove)
+        this.refresh()
+    },
+    clearList() {
+        this.domElement.innerHTML = ""
+    },
+    render() {
+        this.tasks.forEach(task => {
+            const newTaskToDisplay = task.createTaskElement()
+            this.domElement.appendChild(newTaskToDisplay)
+        });
+    },
+    refresh() {
+        this.clearList();
+        this.render();
+    }
+}
+
+buttonAddTask.addEventListener("click", () => {
+    const newTaskLabel = inputNewTaskLabel.value
+    const newTask = new Task(newTaskLabel)
+    taskList.addTask(newTask)
+})
